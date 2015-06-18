98181d8d61df42c989af735808b514a718206f4b
MagGitSoft <magnus_linander@hotmail.com>
2015-06-18 13:27:08 +0200

    Big combat update!
    Thanks to close friend for help! :)

--- ias.py
+++ ias.py
@@ -1,16 +1,29 @@
 # -*- coding: cp1252 -*-
+import sys
 import random
 from tables import *
 import numpy
+from pandas import DataFrame, read_csv
 
 ###''''''''''''''''''''''''''''''''''''''''''###
 ###~~~Definitionary~~~~~~~~~~~~~~~~~~~~~~~~~~###
 ###''''''''''''''''''''''''''''''''''''''''''###
 ''''''''''''''''''''''''
-
+'''
 class StatsTable(IsDescription):
+    #tables.open_file(filename, mode='r', title='',
+    #root_uep='/', filters=None, **kwargs)
+    h5file = open_file("iastables.h5", mode = "w", title = "Test file")
+    #File.create_group(where, name, title='',
+    #filters=None, createparents=False)
+    group = h5file.create_group("/", 'mobinfo', 'Mobstats, playerstats etc.')
+    #ile.create_table(where, name, description=None, title='',
+    #filters=None, expectedrows=10000, chunkshape=None,
+    #byteorder=None, createparents=False, obj=None)
+    table = h5file.create_table()
+        
     #columns
-    '''
+    
     name      = StringCol(16)   # 16-character String
     idnumber  = Int64Col()      # Signed 64-bit integer
     ADCcount  = UInt16Col()     # Unsigned short integer
@@ -18,47 +31,38 @@ class StatsTable(IsDescription):
     grid_i    = Int32Col()      # 32-bit integer
     grid_j    = Int32Col()      # 32-bit integer
     pressure  = Float32Col()    # float  (single-precision)
-    energy    = Float64Col()    # double (double-precision)'''
+    energy    = Float64Col()    # double (double-precision)
 
     idnumber = Int64Col()       #Signed 64-bit integer
     name = StringCol(16)        #16 character string
     surname = StringCol(16)     #--||--
     hpbase = Int32Col()         #32-bit integer
     manabase = Int32Col()       #--||--
-
+    
+    h5file.close()
+    '''
 ''''''''''''''''''''''''
 def dice(sides, throws):
-    rolls = [0] * 7 #rolls = list with 
+    rolls = [0] * 21 #rolls = list with the value 0 in 7 additions
     for i in range(throws):
         
         #4-sided die
         if sides == 4:
             roll = random.randint(1, 4)#random int between (x, y)
             #checks what int is output and adds 1 to corresponding index
-            if roll == 1:
-                rolls[1] += 1
-            if roll == 2:
-                rolls[2] += 1
-            if roll == 3:
-                rolls[3] += 1
-            if roll == 4:
-                rolls[4] += 1
+            if roll in range(1, 5):
+                rolls[roll] += 1
                 
         #6-sided die
         if sides == 6:
             roll = random.randint(1, 6)
-            if roll == 1:
-                rolls[1] += 1
-            if roll == 2:
-                rolls[2] += 1
-            if roll == 3:
-                rolls[3] += 1
-            if roll == 4:
-                rolls[4] += 1
-            if roll == 5:
-                rolls[5] += 1
-            if roll == 6:
-                rolls[6] += 1
+            if roll in range(1, 7):
+                rolls[roll] += 1
+        
+        if sides == 20:
+            roll = random.randint(1, 20)
+            if roll in range(1, 21):
+                rolls[roll] += 1
     
     
     roll_result = []
@@ -78,21 +82,89 @@ def dice(sides, throws):
 #def diceval(rolls):
     
 ''''''''''''''''''''''''
-def monster(species, name):
-    name = name
-    species = species
-    hp = 100
-    atk = dice(4, 1)
-    init = random.randint(1, 20)
-''''''''''''''''''''''''
-def playerstats(name, job):
-    name = name
-    job = job
-    hp = 100
-    atk = dice(6, 1)
-    init = random.randint(1, 20)
+class Monster:
+    def __init__(self, name, species, hp, damage):
+        self.name = name
+        self.species = species
+        self.hp = 100
+        self.damage = 5
+        self.atk = dice(4, 1)
+        self.initiative = 1
+
+    #def unconscious(self):
+        #return self.hp <= 0 and self.hp > -10
     
+    def undefeated(self):
+        return self.hp > 0
+            
 ''''''''''''''''''''''''
+class Player:
+    def __init__(self, name, job):
+        self.name = name
+        self.hp = 100
+        self.mana = 100
+        self.damage = 5
+        self.job = job
+        self.initiative = 1
+    def attackDmg(self):
+        return random.randint(5,20)
+    def alive(self):
+        return self.hp > 0
+        if self.hp <= 0:
+            print ("Du er dævv.")
+            sys.exit("Virkelig, skikkelig dævv...")
+
+''''''''''''''''''''''''
+def combat(player, monster):
+    turn = 1  
+    while 1 == 1:
+        if dice(20, 1)[1][0] + monster.initiative \
+        > dice(20, 1)[1][0] +player.initiative:
+            print ("%s goes first!" % (monster.name))
+            player.hp -= monster.damage
+            print ("%s does ")
+            break
+        elif dice(20, 1)[1][0] + player.initiative \
+        > dice(20, 1)[1][0] + monster.initiative:
+            print ("You go first!")
+            
+    
+    while monster.undefeated() and player.alive():
+        print ("\nTurn: ", turn)
+        print ("Monster health:", monster.hp)
+        print ("Player health: ", player.hp)
+        
+        fightchoice = input("Attack, heal or run?\n")        
+        if fightchoice == ("attack" or "a"):
+            dmgDealt = player.attackDmg()
+            monster.hp -= dmgDealt
+            print ("You dealt", dmgDealt, "damage!")
+            
+        elif fightchoice == "heal":
+            player.hp += 50
+        else:
+            
+            if fightchoice == "run":
+                print ("You ran from the %s" % monster.species)
+                break
+            elif player.alive != True:
+                print ("You've been defeated!")
+                break
+            else:
+                print ("%s is defeated!" % monster.species)
+                break
+        turn += 1
+        
+        
+        monsterDmg = monster.damage
+        player.hp -= monsterDmg
+        print ("%s dealt %s damage to you!" % (monster.species, monsterDmg)) 
+    
+    if monster.undefeated == False and player.alive == True:
+        print ("Congratulations, you won the battle!")
+        
+''''''''''''''''''''''''
+
 # str_choice accepts any string input that matches with 'options'-list in argument
 def str_choice(text, options, complaint): #choice function with three arguments:
                                           #(text, options, complaint)
@@ -175,7 +247,7 @@ jobs = ["bum","mage", "rogue", "warrior"]
 job = str_choice("What's your job? ", jobs, "That's not a job"
                        "! These are jobs: \n")
 
-player = playerstats(name, job)
+player = Player(name, job)
 print (player)
 
 print ("\n 'Hello %s! You're the most shabby-looking %s I've" % (name, job)
@@ -266,4 +338,4 @@ if rabbitcaveopt == 5:
            + " this one looks angrier. It jumps towards you and you slide"
            + " aside to dodge it. You've been attacked, oh my!")
 
-rabbit = monster("barbit", "Barbit")
+rabbit = Monster("Wild Barbit", "Barbit", 100, 5)
